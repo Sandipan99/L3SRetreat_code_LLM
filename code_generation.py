0917c887254057201e5d77997bcb3ac5275e559a
Sandipan Sikdar <sikdarsandipan99@gmail.com>
2024-10-01 11:52:06 +0200

    Create code_generation.py

---
+++ code_generation.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import login
+
+
+m = "bigcode/starcoder"
+login(token='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')
+tokenizer = AutoTokenizer.from_pretrained(m)
+model = AutoModelForCausalLM.from_pretrained(m, device_map='auto')
+
+inputs = tokenizer.encode("def print_hello_world():", return_tensors="pt").to(model.device)
+outputs = model.generate(inputs)
+print(tokenizer.decode(outputs[0]))
